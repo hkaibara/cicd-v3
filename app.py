608d095d1ae5ea30009cdb4567f71522882a8d7f
hkaibara <hiroshi.kaibara.hk@gmail.com>
2025-02-19 13:49:43 +0800

    fix: app mock fix on feat-a

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'Hello, World! This is a test flask another fix happend here'
+    return 'Hello, World! This is a test flask another fix happend here. and another one from feat-a'
 if __name__ == '__main__':
     app.run(debug=True)
     print(os.environ['env_var1'])
